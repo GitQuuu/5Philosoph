85a0e3aa77b66f55bce0c4328f29488d56054d13
Qu <Quan0027@elec.techcollege.dk>
2020-01-31 11:09:02 +0100

    Threading now works and printet to console

---
+++ 5Philosoph/Philosoph.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Philosoph
+{
+    class Philosoph
+    {
+        public string  Name { get; set; }
+        public int EatTime { get; set; }
+        public int ThinkTime { get; set; }
+
+        private bool left = false;
+        private bool right = false;
+
+
+        public Philosoph()
+        {
+            
+        }
+
+        public Philosoph(string name,int eatTime, int thinkTime)
+        {
+            Name = name;
+            EatTime = eatTime;
+            ThinkTime = thinkTime;
+        }
+
+      
+
+        public static void SørenKirkegaard()
+        {
+            new Philosoph("Søren Kirkegård", 0, 0);
+
+            while (true)
+            {
+               // keep the thread alive 
+            }
+        }
+        public static void Platon()
+        {
+            new Philosoph("Platon", 0, 0);
+
+            while (true)
+            {
+                // keep the thread alive 
+            }
+        }
+        public static void ImmanuelKant()
+        {
+            new Philosoph("Immanuel Kant", 0, 0);
+
+            while (true)
+            {
+                // keep the thread alive 
+            }
+        }
+        public static void Aristokeles()
+        {
+            new Philosoph("Aristokeles", 0, 0);
+
+            while (true)
+            {
+                // keep the thread alive 
+            }
+        }
+        public static void JeanPaulSartre()
+        {
+            new Philosoph("Jean-Paul Sartre", 0, 0);
+
+            while (true)
+            {
+                // keep the thread alive 
+            }
+        }
+    }
+}
--- 5Philosoph/Program.cs
+++ 5Philosoph/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _5Philosoph
@@ -14,8 +18,43 @@ namespace _5Philosoph
              mellem hver af dem ligger der en gaffel 5 i alt, Filosofferne samler en gaffel op og beholder den til den anden er ledig når en filosof har 2 gafler spiser han for en stund og lægger gaflerne ned 
              det munder ud i en deadlock og den skal i skabe*/
 
+            ObservableCollection<Thread> threadTracker = new ObservableCollection<Thread>();
+
+            Thread firstThread = new Thread(Philosoph.SørenKirkegaard);
+
+            Thread secondThread = new Thread(Philosoph.Aristokeles);
+
+            Thread thirdThread = new Thread(Philosoph.ImmanuelKant);
+
+            Thread fourthThread = new Thread(Philosoph.JeanPaulSartre);
+
+            Thread fiftThread = new Thread(Philosoph.Platon);
 
 
+            firstThread.Start();
+            threadTracker.Add(firstThread);
+
+            secondThread.Start();
+            threadTracker.Add(secondThread);
+
+            thirdThread.Start();
+            threadTracker.Add(thirdThread);
+
+            fourthThread.Start();
+            threadTracker.Add(fourthThread);
+
+            fiftThread.Start();
+            threadTracker.Add(fiftThread);
+
+            foreach (Thread threads in threadTracker)
+            {
+                Console.WriteLine($"Thread: {threads.ManagedThreadId} State: {threads.ThreadState} Priority: {threads.Priority}");
+                
+            }
+
+
+
+            Console.Read();
         }
     }
 }
