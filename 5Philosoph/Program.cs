3a6e5084579c2e59014e11238877251fdbf2236b
Qu <Quan0027@elec.techcollege.dk>
2020-01-31 09:11:10 +0100

    Add case in comments

---
+++ 5Philosoph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Philosoph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 5 stille filosoffer sidder og spekluere rundt om et bord
+             mellem hver af dem ligger der en gaffel 5 i alt, Filosofferne samler en gaffel op og beholder den til den anden er ledig når en filosof har 2 gafler spiser han for en stund og lægger gaflerne ned 
+             det munder ud i en deadlock og den skal i skabe*/
+
+
+
+        }
+    }
+}
