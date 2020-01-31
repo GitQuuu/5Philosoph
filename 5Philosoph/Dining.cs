269d55017e06e220ca42fb0cb590f56beabf160a
Qu <Quan0027@elec.techcollege.dk>
2020-01-31 11:28:54 +0100

    making code more clean

---
+++ 5Philosoph/Dining.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _5Philosoph
+{
+    class Dining
+    {
+
+        public static ObservableCollection<Thread> threadTracker = new ObservableCollection<Thread>();
+        public static void ThreadStarter()
+        {
+            Console.WriteLine("Overview over threads\n");
+
+            Thread firstThread = new Thread(Philosoph.SørenKirkegaard);
+
+            Thread secondThread = new Thread(Philosoph.Aristokeles);
+
+            Thread thirdThread = new Thread(Philosoph.ImmanuelKant);
+
+            Thread fourthThread = new Thread(Philosoph.JeanPaulSartre);
+
+            Thread fiftThread = new Thread(Philosoph.Platon);
+
+            firstThread.Start();
+            threadTracker.Add(firstThread);
+
+            secondThread.Start();
+            threadTracker.Add(secondThread);
+
+            thirdThread.Start();
+            threadTracker.Add(thirdThread);
+
+            fourthThread.Start();
+            threadTracker.Add(fourthThread);
+
+            fiftThread.Start();
+            threadTracker.Add(fiftThread);
+
+            foreach (Thread threads in threadTracker)
+            {
+                Console.WriteLine($"Thread: {threads.ManagedThreadId} State: {threads.ThreadState} Priority: {threads.Priority}");
+
+            }
+        }
+      
+    }
+}
--- 5Philosoph/Program.cs
+++ 5Philosoph/Program.cs
@@ -18,39 +18,9 @@ namespace _5Philosoph
              mellem hver af dem ligger der en gaffel 5 i alt, Filosofferne samler en gaffel op og beholder den til den anden er ledig når en filosof har 2 gafler spiser han for en stund og lægger gaflerne ned 
              det munder ud i en deadlock og den skal i skabe*/
 
-            ObservableCollection<Thread> threadTracker = new ObservableCollection<Thread>();
 
-            Thread firstThread = new Thread(Philosoph.SørenKirkegaard);
+            Dining.ThreadStarter();
 
-            Thread secondThread = new Thread(Philosoph.Aristokeles);
-
-            Thread thirdThread = new Thread(Philosoph.ImmanuelKant);
-
-            Thread fourthThread = new Thread(Philosoph.JeanPaulSartre);
-
-            Thread fiftThread = new Thread(Philosoph.Platon);
-
-
-            firstThread.Start();
-            threadTracker.Add(firstThread);
-
-            secondThread.Start();
-            threadTracker.Add(secondThread);
-
-            thirdThread.Start();
-            threadTracker.Add(thirdThread);
-
-            fourthThread.Start();
-            threadTracker.Add(fourthThread);
-
-            fiftThread.Start();
-            threadTracker.Add(fiftThread);
-
-            foreach (Thread threads in threadTracker)
-            {
-                Console.WriteLine($"Thread: {threads.ManagedThreadId} State: {threads.ThreadState} Priority: {threads.Priority}");
-                
-            }
 
 
 
